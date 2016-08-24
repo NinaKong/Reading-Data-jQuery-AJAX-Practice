81c2f3c40642944f38eca082e4f264ea06d43faf
NinaSleephead <kx1214@gmail.com>
2016-08-23 21:45:07 -0400

    Reading-Data-jQuery-AJAX-Practice
    Practice for reading data with jQuery in AJAX

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$getJSON('data/data.json', function(data){
+    var result = '<ul>';
+    $.each(data, function (key, val) {
+        output += '<li>' + val.shortname + '</li>';
+    });
+    result += '</ul>';
+    $('#readData').prepemd(result);
+});
+
